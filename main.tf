f3d8428028369eb7213a2118ac68c214879dba8d
zerotohero200222 <aebharath17@gmail.com>
2025-07-03 16:06:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aks${random_string.dns.result}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "random_string" "dns" {
+  length  = 6
+  special = false
+  upper   = false
+}
